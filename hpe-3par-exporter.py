a5838f6b2d77b379bc50a9e5b50b2b74224c919b
charlesqing <1203561895@qq.com>
2025-01-10 15:27:15 +0800

    Update hpe-3par-exporter.py
    Add the prefix hpe_

--- hpe-3par-exporter.py
+++ hpe-3par-exporter.py
@@ -92,39 +92,39 @@ class HP3PARCollector:
 
         # 生成唯一标识符，并替换非法字符
         identifier = device_id or element_name or tag or serial_number
-        identifier = identifier.replace('.', '_').replace('[', '_').replace(']', '_').replace('-', '_').replace(' ', '_')  # 替换空格为下划线
+        identifier = identifier.replace('.', '_').replace('[', '_').replace(']', '_').replace('-', '_').replace(' ', '_') 
 
         # 添加健康状态指标
         if "HealthState" in instance and instance["HealthState"] is not None:
-            key = f"health_{cim_class[4:]}_{identifier}"
+            key = f"hpe_health_{cim_class[4:]}_{identifier}"
             metrics[key] = GaugeMetricFamily(key, f'Health State of {cim_class}')
             metrics[key].add_metric([], float(instance["HealthState"]))
 
         # 添加操作状态指标
         if "OperationalStatus" in instance and instance["OperationalStatus"] and instance["OperationalStatus"][0] is not None:
-            key = f"oper_{cim_class[4:]}_{identifier}"
+            key = f"hpe_oper_{cim_class[4:]}_{identifier}"
             metrics[key] = GaugeMetricFamily(key, f'Operational State of {cim_class}')
             metrics[key].add_metric([], float(instance["OperationalStatus"][0]))
 
         # 处理电池容量和电压
         if cim_class == 'TPD_Battery':
             if "RemainingCapacity" in instance and instance["RemainingCapacity"] is not None:
-                key = f"battery_capacity_{identifier}"
+                key = f"hpe_battery_capacity_{identifier}"
                 metrics[key] = GaugeMetricFamily(key, 'Remaining Capacity of Battery')
                 metrics[key].add_metric([], float(instance["RemainingCapacity"]))
             if "Voltage" in instance and instance["Voltage"] is not None:
-                key = f"battery_voltage_{identifier}"
+                key = f"hpe_battery_voltage_{identifier}"
                 metrics[key] = GaugeMetricFamily(key, 'Voltage of Battery')
                 metrics[key].add_metric([], float(instance["Voltage"]))
 
         # 处理系统 LED 和其他操作状态
         if cim_class == 'TPD_NodeSystem' and "SystemLED" in instance and instance["SystemLED"] is not None:
-            key = f"led_{cim_class[4:]}_{identifier}"
+            key = f"hpe_led_{cim_class[4:]}_{identifier}"
             metrics[key] = GaugeMetricFamily(key, f'LED State of {cim_class}')
             metrics[key].add_metric([], float(instance["SystemLED"]))
 
         if cim_class in ['TPD_SASPort', 'TPD_FCPort', 'TPD_EthernetPort'] and "OtherOperationalStatus" in instance and instance["OtherOperationalStatus"] is not None:
-            key = f"other_oper_{cim_class[4:]}_{identifier}"
+            key = f"hpe_other_oper_{cim_class[4:]}_{identifier}"
             metrics[key] = GaugeMetricFamily(key, f'Other Operational State of {cim_class}')
             metrics[key].add_metric([], float(instance["OtherOperationalStatus"]))
 
@@ -138,7 +138,7 @@ class HP3PARCollector:
                 stdin, stdout, stderr = ssh_client.exec_command(f'showspace -cpg {cpg_name}')
                 output = stdout.read().decode("utf-8").split("\n")[3]
                 overprv_value = float(output.split()[-1])
-                key = f"overprv_DynamicStoragePool_{cpg_name.replace('.', '_').replace(' ', '_')}"  # 替换空格为下划线
+                key = f"hpe_overprv_DynamicStoragePool_{cpg_name.replace('.', '_').replace(' ', '_')}"
                 metrics[key] = GaugeMetricFamily(key, f'Overprovisioning of {cpg_name}')
                 metrics[key].add_metric([], overprv_value)
         except Exception as e:
